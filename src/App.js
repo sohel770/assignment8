4344d4dd854806431d72404edc08ed12af1cfc31
sohel Ahamed <sohel770hossain@gmail.com>
2022-03-27 15:30:24 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div>
+      <Shop/>
     </div>
   );
 }
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+
+    return (
+        
+        <div className='shop-container'>
+           <div className="product-container">
+<h3>heel</h3>
+
+           </div>
+           <div className="cart-container">
+        <h3>Order Summary</h3>
+
+           </div>
+        </div>
+    );
+};
+
+export default Shop;
